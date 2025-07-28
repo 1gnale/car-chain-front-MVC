97bf8d4cd0b7d73abf48e122ff58d8150834a8f1
JuanCarriles <juanmcarriles@gmail.com>
2025-07-28 15:36:36 -0300

    Intento 1 de subir el archivo

--- src/views/components/FormDataClient.tsx
+++ src/views/components/FormDataClient.tsx
@@ -6,7 +6,6 @@ import { useAppSelector } from "../../redux/reduxTypedHooks";
 import SelectForm from "./GeneralComponents/SelectForm.tsx";
 import GrayButton from "./GeneralComponents/Button.tsx";
 import useLocalStorageItem from "../../controllers/controllerHooks/LocalStorage/getFromLocalStorageHook.ts";
-import type { Cliente, Localidad, Provincia, Vehiculo } from "../../models/types.d.ts";
 
 const FormDataClient = ({
   handleCurrentView,
