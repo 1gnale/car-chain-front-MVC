5c2299a2fec1c5525fdc27340ca14ea1b3cd4e07
1gnale <nosoyignacio@gmail.com>
2025-07-30 15:01:04 -0300

    Added protected route to /perfil & bases to each view at /perfil

---
+++ src/views/components/FormAccountData/AccountPolicy.tsx
@@ -0,0 +1,12 @@
+import TitleForm from "../GeneralComponents/TitleForm";
+
+const AccountPolicy = () => {
+    return (
+        <div className="col-xl-9">
+            <TitleForm title="Polizas" />
+            <p>Labura aca kuscoa</p>
+        </div>
+    );
+};
+
+export default AccountPolicy;
